4adae1972f8f2db6fd51eeb2024480b5e698f103
YuNiGor <124684684+YuNiGor@users.noreply.github.com>
2023-12-11 18:39:55 +0100

    404 added

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import saleImage from '../../assets/sale_new_season.png';
+import notFoundImage from '../../assets/404.svg';
+import styles from './NotFound.module.css';
 
 function NotFound() {
-    return  <div className="sale-image">
-                <img src={saleImage} alt="404 Not Found" />
-            </div>;
+  return (
+    <div className={styles.notFoundContainer}>
+      <img src={notFoundImage} alt="Page Not Found" className={styles.notFoundImage} />
+    </div>
+  );
 }
 
 export default NotFound;
